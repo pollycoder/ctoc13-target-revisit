e5e8e53f59e7f79976f5509e08766522ba1a637b
pollycoder <pollyjoe2003@gmail.com>
2024-11-12 14:13:45 +0800

    update mac

--- CTOC13-C/analysis/single_sat_total.py
+++ CTOC13-C/analysis/single_sat_total.py
@@ -2,8 +2,6 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.colors import Normalize, ListedColormap, BoundaryNorm
 
 '''
 Analysis of single satellite data
@@ -32,16 +30,12 @@ def read_data(db_filepath):
     data = data.reshape(-1, 7)
     return data
 
-def custom_colormap():
-    cmap = (ListedColormap(['blue', 'royalblue', 'lightgreen', 'yellow', 'orange'])
-        .with_extremes(over='red'))
-    
-    return cmap
+
 
 
 if __name__ == '__main__':
-    # Save data
-    data_name = 'single_sat_ship'
+     # Save data
+    data_name = 'single_sat_1108_2_large'
     # data_bin_path = os.path.join(os.path.dirname(__file__), '../data', data_name + '.bin')
     # data = read_data(data_bin_path)
     # np.save(os.path.join(os.path.dirname(__file__), '../data', data_name + '.npy'), data)
@@ -52,7 +46,7 @@ if __name__ == '__main__':
     print(data.shape)
 
     # Analysis
-    step = 100
+    step = 50
     sma = data[0:data.shape[0]:step, 0]
     inc = np.rad2deg(data[0:data.shape[0]:step, 2])
     raan = np.rad2deg(data[0:data.shape[0]:step, 3])
@@ -63,23 +57,13 @@ if __name__ == '__main__':
     # Color bar shown on the right
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
-    
-    # 创建自定义颜色映射
-    custom_cmap = custom_colormap()
-    
-    # 使用自定义的颜色映射和归一化
-    bounds = np.linspace(0, 8, 5)
-    norm = BoundaryNorm(bounds, custom_cmap.N, extend='max')
-    
-    scatter = ax.scatter(sma, inc, raan, c=n_visible, cmap=custom_cmap, norm=norm)
-    
-    ax.set_xlabel('sma(km)')
+    ax0 = ax.scatter(sma, inc, raan, c=n_visible, cmap='jet')
+
+    ax.set_xlabel('sma')
     ax.set_ylabel('inc(deg)')
     ax.set_zlabel('raan(deg)')
 
-    fig.colorbar(scatter)
-    plt.title('sma & inc & raan vs n_ship_visible (max 8)')
+    fig.colorbar(ax0)
+    plt.title('sma & inc & raan vs n_visible')
    
     plt.show()
-
-
